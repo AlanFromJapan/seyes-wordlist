6db886b956d36972d3f62494135bbc7b204417b5
AlanFromJapan <alanfromjapan@gmail.com>
2025-05-09 22:21:59 +0900

    Added program to generate small math equations

---
+++ src/seyes_maths.py
@@ -0,0 +1,101 @@
+from seyes_wordlist import generate_page
+import sys
+import random
+
+PLACEHOLDER = "__"
+
+
+def math_addition(max_number=100):
+    a = random.randint(1, 100)
+    b = min(max_number -a, random.randint(1, 100))
+    c = a + b
+
+    lbl = [a, b, c]
+    #clear one
+    lbl[random.randint(0, 2)] = PLACEHOLDER
+
+    return f"{lbl[0]} + {lbl[1]} = {lbl[2]}"
+
+
+def math_substraction(min_number=0, max_number=100):
+    a = random.randint(1, max_number)
+    #dirty way to avoid x - x = 0 ... not very challenging
+    b = a
+    while b == a:
+        b = min(min_number + a, random.randint(1, max_number))
+    c = a - b
+
+    lbl = [a, b, c]
+    #clear one
+    lbl[random.randint(0, 2)] = PLACEHOLDER
+
+    return f"{lbl[0]} - {lbl[1]} = {lbl[2]}"
+
+
+def math_multiplication(max_number=10):
+    a = random.randint(1, max_number)
+    b = random.randint(1, max_number)
+    c = a * b
+
+    lbl = [a, b, c]
+    #clear one
+    lbl[random.randint(0, 2)] = PLACEHOLDER
+
+    return f"{lbl[0]} × {lbl[1]} = {lbl[2]}"
+
+
+def math_division(max_number=10):
+    #avoid x / 1 = x ... not very challenging
+    a = random.randint(2, max_number)
+    #dirty way to avoid x / x = 1 ... not very challenging
+    b = random.randint(2, max_number)
+
+    #ok, copilot, nice ...
+    c = a * b
+
+    lbl = [c, a, b]
+    #clear one
+    lbl[random.randint(0, 2)] = PLACEHOLDER
+
+    return f"{lbl[0]} / {lbl[1]} = {lbl[2]}"
+
+
+"""Weights must sum to 100"""
+def generate_equations(count=10, weight_addition=30, weight_substraction=30, weight_multiplication=20):
+    equations = []
+    for _ in range(count):
+        #randomly choose an operation
+        op = random.randint(0, 100)
+        if op < weight_addition:
+            equations.append(math_addition())
+        elif op < weight_addition + weight_substraction:
+            equations.append(math_substraction())
+        elif op < weight_addition + weight_substraction + weight_multiplication:
+            equations.append(math_multiplication())
+        else:
+            equations.append(math_division())
+
+    return equations
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("""
+Usage: python3 seys_maths.py output_file [repeat]
+    Generates a page of maths exercises generated randomly file.
+        """)
+        exit(1)
+    
+    if len(sys.argv) >= 2:
+        repeat = False
+        if len(sys.argv) >= 3:
+            repeat = str(sys.argv[2]).lower() == "repeat"
+
+        output_file = sys.argv[1] 
+        if not output_file.lower().endswith(".png"):
+            output_file = output_file[:-4] + ".png"
+
+        #generate 35 equations, with mostly addition and substraction (left 10% for division)
+        lines = generate_equations(count=35, weight_addition=35, weight_substraction=35, weight_multiplication=20)
+
+        generate_page(lines, sys.argv[1] , repeat)
--- src/unit_tests.py
+++ src/unit_tests.py
@@ -1,29 +1,61 @@
 import unittest
 from seyes_wordlist import generate_page
+import seyes_maths
 
 class TestSeyesWordlist(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_generate_page(self):
-        # Test if generate_page returns a PIL Image
-        generate_page(
-            ["il y a", "un jardin", "quatre", "du chocolat", "un garçon", "alors", "après", "je m'appelle", "les vacances"], 
-            path_to_save="test01.png"
-        )
+    # def test_generate_page(self):
+    #     # Test if generate_page returns a PIL Image
+    #     generate_page(
+    #         ["il y a", "un jardin", "quatre", "du chocolat", "un garçon", "alors", "après", "je m'appelle", "les vacances"], 
+    #         path_to_save="test01.png"
+    #     )
 
-        generate_page(
-            [], 
-            path_to_save="test02.png"
-        )
+    #     generate_page(
+    #         [], 
+    #         path_to_save="test02.png"
+    #     )
 
-        generate_page(
-            ["AAAAAAAAAAAAAAAAAAAAA", "AAAAAAAAAAAAAAAA", "bbbbbbbbbbbbbbb", "hhhhhhhhhhhhhhhhh", "qqqqqqqqqqqqq", "ggggggggggg"], 
-            path_to_save="test03.png"
-        )
+    #     generate_page(
+    #         ["AAAAAAAAAAAAAAAAAAAAA", "AAAAAAAAAAAAAAAA", "bbbbbbbbbbbbbbb", "hhhhhhhhhhhhhhhhh", "qqqqqqqqqqqqq", "ggggggggggg"], 
+    #         path_to_save="test03.png"
+    #     )
         
 
+    def test_math_addition(self):
+        # Test if math_addition returns a string
+        result = seyes_maths.math_addition()
+        print(result)
+        self.assertIsInstance(result, str)
+        self.assertIn("+", result)
+        self.assertIn("=", result)
+
+    def test_math_substraction(self):
+        # Test if math_substraction returns a string
+        result = seyes_maths.math_substraction()
+        print(result)
+        self.assertIsInstance(result, str)
+        self.assertIn("-", result)
+        self.assertIn("=", result)
+
+    def test_math_multiplication(self):
+        # Test if math_multiplication returns a string
+        result = seyes_maths.math_multiplication(max_number=5)
+        print(result)
+        self.assertIsInstance(result, str)
+        self.assertIn("×", result)
+        self.assertIn("=", result)
+
+    def test_math_division(self):
+        # Test if math_division returns a string
+        result = seyes_maths.math_division(max_number=5)
+        print(result)
+        self.assertIsInstance(result, str)
+        self.assertIn("/", result)
+        self.assertIn("=", result)
 
 if __name__ == '__main__':
     unittest.main()
